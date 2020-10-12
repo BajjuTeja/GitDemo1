0e0882beb5b86acb149d550fe3531776fec835fe
teja651 <tejabajju@gmail.com>
2020-10-12 16:06:06 +0530

    First Commit .......

---
+++ src/com/cap/GitDemo.java
@@ -0,0 +1,16 @@
+package com.cap;
+
+public class GitDemo {
+
+	
+	static String m = "Hello World";
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(m.substring(1));
+	}
+	
+	
+	
+}
